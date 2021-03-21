c8ebce7b09a853f4da05ed37dcc8fa60eeec2ad9
DeepaNair106 <72653999+DeepaNair106@users.noreply.github.com>
2021-03-21 16:33:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier("MobileNet" , modelLoaded);
+
+  previous_result = "";
+}
+
+function modelLoaded(){
+  console.log("Model Loaded");
+}
+
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video , gotResult);
+}
+
+
+function gotResult(error , results){
+   
+if(error){
+  console.error(error);
+}
+else{
+  if(( results[0].confidence > 0.5) && (results[0].label!= previous_result)){
+    console.log(results);
+    previous_result = results[0].label;
+    var synth = window.speechSynthesis;
+    speak_data = "Object identified is " + results[0].label;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    document.getElementById("object_name").innerHTML = results[0].label;
+    document.getElementById("object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
+
+}
+
+
